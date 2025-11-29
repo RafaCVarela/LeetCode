605b92c52729827955a4ea1cffdb257f5b10303e
Rafael Costa Varela <48953925+RafaCVarela@users.noreply.github.com>
2025-11-29 10:58:51 -0300

    Add solution for minimum operations to make sum divisible by k
    Implement a solution to find the minimum operations required to make the sum of an array divisible by a given integer k.

---
+++ Minimum Operations to Make Array Sum Divisible by K/php/v1/code.php
@@ -0,0 +1,58 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $k
+     * @return Integer
+     */
+    
+    public $count = 0;
+
+    function minOperations($nums, $k) {
+        
+        // $len = count($nums);
+        // echo $this->sum($nums)%$k;
+
+        $sum = $this->sum($nums);
+        
+        if($sum%$k === 0){
+            return $this->count;
+        } elseif ($sum < $k){
+            return $sum + $this->count;
+        } else{
+            $index = array_search(max($nums), $nums);
+            $nums[$index] -= 1;
+
+            $this->count += 1;
+            return $this->minOperations($nums, $k);
+        }
+        
+    }
+
+    function sum($nums){
+
+        $sum = 0;
+
+        foreach($nums as $num){
+
+            $sum += $num;
+
+        }
+
+        return $sum;
+    }
+}
+
+
+// English - For tests
+// Português - Para teste rode;
+$codeObject = new Solution();
+
+$answer = $codeObject->minOperations([5, 10, 15], 5);
+$answer = $codeObject->minOperations([3, 2], 6);
+$answer = $codeObject->minOperations([3, 9, 7], 5);
+$answer = $codeObject->minOperations([4, 1, 3], 4);
+
+echo $answer;
